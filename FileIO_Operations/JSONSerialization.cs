a3fcc5d16ea5d1b5edee2874b3cd00902c0ea675
Prasad <banprasad21@gmail.com>
2022-09-12 08:18:01 +0530

    [Raghav] Add: File Operations with JSON serialization

---
+++ FileIO_Operations/JSONSerialization.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO_Operations
+{
+    internal class JSONSerialization
+    {
+        public void JsonSerialize()
+        {
+            BlogSite blogsite = new BlogSite()
+            {
+                ApplicationName = "Prasad",
+                ApplicationId = 298
+            };
+            string jsonData = JsonConvert.SerializeObject(blogsite);
+            Console.WriteLine(jsonData);
+        }
+
+        public void JsonDeSerialize()
+        {
+            string json = @"{
+                'ApplicationName':'Prasad',
+                'ApplicationId': 298
+            }";
+            BlogSite blogsite = JsonConvert.DeserializeObject<BlogSite>(json);
+            Console.WriteLine("Applicant Name: " + blogsite.ApplicationName + "\n" + "Applicant ID: " + blogsite.ApplicationId);
+        }
+
+    }
+
+
+    [DataContract]
+    class BlogSite
+    {
+        [DataMember]
+        public string ApplicationName { get; set; }
+        [DataMember]
+        public int ApplicationId { get; set; }
+    }
+}
+}
---
+++ FileIO_Operations/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+namespace FileIO_Operations
+{
+    class Program
+    {
+        public static void FileExist()
+        {
+            string path = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperations.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadKey();
+        }
+        public static void ReadLines()
+        {
+            string path = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperations.txt";
+            string[] line;
+            line = File.ReadAllLines(path);
+
+            Console.WriteLine(line[0]);
+            Console.WriteLine(line[1]);
+            Console.ReadKey();
+        }
+        public static void ReadAllText()
+        {
+            string path = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperations.txt";
+            string line;
+            line = File.ReadAllText(path);
+            Console.WriteLine(line);
+            Console.ReadKey();
+        }
+        public static void CopyFile()
+        {
+            string path = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperations.txt";
+            string Copypath = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperationsCopy.txt";
+            File.Copy(path, Copypath);
+            Console.ReadKey();
+        }
+        public static void DeleteFile()
+        {
+            string path = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperationsCopy.txt";
+            File.Delete(path);
+            Console.ReadKey();
+        }
+        public void StreamReader()
+        {
+            string path = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperations.txt";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+        public void StreamWriter()
+        {
+            string path = @"C:\Users\banpr\Desktop\FileOperations\FileIO\FileOperations.txt";
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                sr.WriteLine("Stream Writing");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+                Console.ReadKey();
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("File IO operations");
+            //Console.WriteLine("\nFile Exists Operation ");
+            //FileExist();
+            //Console.WriteLine("\nRead lines operation");
+            //ReadLines();
+            //Console.WriteLine("\nRead All text operation");
+            //ReadAllText();
+            //Console.WriteLine("\nCopy File Operation");
+            //CopyFile();
+            //Console.WriteLine("\nDelete File Operation");
+            //DeleteFile();
+
+
+            //Program program = new Program();
+            //program.StreamReader();
+            // program.StreamWriter();
+        }
+    }
+}
